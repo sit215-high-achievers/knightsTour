cc0c2a27c66423de66c83abc1f5db5c652f68207
Ian-RG <30923025+Ian-RG@users.noreply.github.com>
2019-08-09 18:36:06 +1000

    Launches with parameters, detects closed tours

--- knightsTour.cpp
+++ knightsTour.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <ctime>
+#include <string>
 #include "treeNode.h"
-
-#define BOARD_SIZE 9
 
 void printBoard(const std::vector<std::vector<int>> &board)
 {
@@ -16,14 +16,46 @@ void printBoard(const std::vector<std::vector<int>> &board)
     std::cout << std::endl;
 }
 
-int main()
+void usage()
 {
-    //Initialise board as a 2d vector filled with 0s
-    std::vector<std::vector<int>> board(BOARD_SIZE, std::vector<int>(BOARD_SIZE));
-    //Create first node in tree with starting coordinates of the knight
-    TreeNode current(0, 0, board);
-    //Process first node. processNode is called recursively on child nodes
-    TreeNode::processNode(current, 1, board);
-    //Print board after solution is found, or all paths exhausted
-    printBoard(board);
+    std::cout << "Usage: ./knightsTour.exe {board_size(int)} {1 for closed tour, 0 for first found tour}" << std::endl;
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    //If correct parameters not passed on startup, present message to user and quit
+    if (argc != 3)
+        usage();
+    else
+    {
+        int boardSize;        
+        std::random_device rd;
+        //Get board size from arguments, convert char array to int
+        std::string bs(argv[1]);
+        std::stringstream str(bs);
+        str >> boardSize;
+        //If the board size isn't valid, present usage message and quit
+        if (!str)
+            usage();
+        //Get closed tour requirement from arguments
+        if (*argv[2] == '1')
+            TreeNode::closedTourRequired = true;
+
+        //Initialise board as a 2d vector filled with 0s
+        std::vector<std::vector<int>> board(boardSize, std::vector<int>(boardSize));
+
+        //Create first node in tree with starting coordinates of the knight
+        TreeNode current(4, 4, board);
+
+        //Process first node. processNode is called recursively on child nodes
+        auto stamp = std::clock();
+        TreeNode::processNode(current, 1, board);
+
+        //Print board after solution is found, or all paths exhausted
+        printBoard(board);
+        std::cout << "Board size: " << boardSize << "\n";
+        std::cout << "Closed tour: " << std::boolalpha << TreeNode::closedTourRequired << "\n";
+        std::cout << "Tour completed in: " << std::clock() - stamp << "ms" << std::endl;
+    }
 }
